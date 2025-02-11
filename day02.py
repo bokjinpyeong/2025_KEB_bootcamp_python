13bb38976fdabe4a9fb98a6856fc02d2309433f3
bokjinpyeong <tapebok@inha.edu>
2025-02-11 10:48:23 +0900

    v0.5 gugudanplus

--- day02.py
+++ day02.py
@@ -1,6 +1,15 @@
-for dan in range(2, 10, 1):
-    for i in range(1, 10, 1):
-        print(f"{dan}*{i} = {dan*i}")
+# for dan in range(2, 10, 1):
+#     for i in range(1, 10, 1):
+#         print(f"{dan}*{i} = {dan*i}")
 
+n = int(input("Input number:"))
+count = 0
+for i in range(1, n+1):
+    if n % i ==0:
+        count = count + 1
 
+if count ==2:
+    print(f"{n} is prime number")
+else:
+    print(f"{n} is not prime number!")
 
