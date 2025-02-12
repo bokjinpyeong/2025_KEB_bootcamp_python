8b776c3386b1920fd8d1587de4036477987587b8
bokjinpyeong <tapebok@inha.edu>
2025-02-12 17:30:53 +0900

    v2.2

--- day03.py
+++ day03.py
@@ -1,30 +1,37 @@
+# Assignment
+# v2.4) 메뉴 삭제 추가에 대응되는 코드를 추가하시오.
 import random
-drinks = ["위스키", "와인", "소주", "고량주" ]
-foods = ["초콜릿", "치즈", "삼겹살", "양꼬치"]
+drinks = ["위스키", "와인", "소주", "고량주"]
+snacks = ["초콜릿", "치즈", "삽겹살", "양꼬치"]
 
-drinks.insert(4,"사케")
-foods.insert(4,"광어회")
-a="ㅇ칸녕"
+drinks.append("사케")
+snacks.append("광어회")
+snacks[0] = "낙곱새"
+# drinks.append("데킬라")
+# snacks.append("소금")
 
-print(len(a))
+menu_list = '다음 술중에 고르세요.\n'
+for i in range(len(drinks)):
+    menu_list = menu_list + f'{i+1}) {drinks[i]}  '
+menu_list = menu_list + f'{len(drinks)+1}) 아무거나  {len(drinks)+2}) 종료 : '
+#print(menu_list)
 
+# f'다음 술중에 고르세요.\n1) {drinks[0]}   2) {drinks[1]}   3) {drinks[2]}   4) {drinks[3]}   5) {drinks[4]}   6) 아무거나   7) 종료 : '
 while True:
-    ramdom_number = random.randint(0, len(drinks)-1)
-    random_drinks = drinks[ramdom_number]
-    ramdom_foods = foods[ramdom_number]
-    menu = input(f'다음 술중에 고르세요.\n1) {drinks[0]}   2) {drinks[1]}   3) {drinks[2]}   4) {drinks[3]}   5) {drinks[4]} 6) random 7) 종료 : ')
+    menu = input(menu_list)
     if menu == '1':
-        print(f'{drinks[0]}에 어울리는 안주는 {foods[0]} 입니다')
+        print(f'{drinks[0]}에 어울리는 안주는 {snacks[0]} 입니다')
     elif menu == '2':
-        print(f'{drinks[1]}에 어울리는 안주는 {foods[1]} 입니다')
+        print(f'{drinks[1]}에 어울리는 안주는 {snacks[1]} 입니다')
     elif menu == '3':
-        print(f'{drinks[2]}에 어울리는 안주는 {foods[2]} 입니다')
+        print(f'{drinks[2]}에 어울리는 안주는 {snacks[2]} 입니다')
     elif menu == '4':
-        print(f'{drinks[3]}에 어울리는 안주는 {foods[3]} 입니다')
+        print(f'{drinks[3]}에 어울리는 안주는 {snacks[3]} 입니다')
     elif menu == '5':
-        print(f'{drinks[4]}에 어울리는 안주는 {foods[4]} 입니다')
+        print(f'{drinks[4]}에 어울리는 안주는 {snacks[4]} 입니다')
     elif menu == '6':
-        print(f'{random_drinks}에 어울리는 안주는 {ramdom_foods} 입니다')
+        random_index = random.randint(0, len(drinks)-1)
+        print(f'{drinks[random_index]}에 어울리는 안주는 {snacks[random_index]} 입니다')
     elif menu == '7':
         print(f'다음에 또 오세요')
         break
