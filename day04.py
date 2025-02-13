0e3be3c002d08a43830de173f877be9c6a26157c
bokjinpyeong <tapebok@inha.edu>
2025-02-13 14:28:55 +0900

    v3.3 day 04 ocp

--- day04.py
+++ day04.py
@@ -1,10 +1,27 @@
-#SOLID
-#open closed principle: 개방 폐쇄 원칙(확장에는 열려 있고 수정에는 닫혀 )있는 원칙
+# SOLID
+# Open Closed Principle : 개방 폐쇄 원칙 (확장에는 열려 있고 수정에는 닫혀있는 원칙)
+import time
+
+
+def time_decorator(func):
+    def wrapper(*arg):
+        s = time.time()
+        r = func(*arg)
+        e = time.time()
+        print(f'실행시간 : {e - s}초')
+        return r
+    return wrapper
+
+@time_decorator
 def factorial_repetition(n) -> int:
     result = 1
     for i in range(2, n+1):
         result = result * i
     return result
-    v3.2 day 04 ocp
+
+
 number = int(input())
-print(f"{number}!={factorial_repetition(number)}")
+# s = time.time()
+print(f"{number}! = {factorial_repetition(number)}")
+# e = time.time()
+# print(e-s)
